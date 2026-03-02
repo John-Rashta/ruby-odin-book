167d1dda7518fc069e77f3751dac51f0ca8ec4b6
John-Rashta <john_rashta@outlook.com>
2026-03-02 21:28:49 +0000

    feat: start testing

--- test/controllers/followships_controller_test.rb
+++ test/controllers/followships_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
+# TEST SUCCESS/FAILURE ON CREATING/DELETING - VIEWS WILL SEE LATER
 class FollowshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  test "Get Followers" do
+    sign_in users(:one)
+    get followships_followers_url
+    assert_response :success
+  end
+
+  test "Get Followings" do
+    sign_in users(:three)
+    get followships_url
+    assert_response :success
+  end
 end
--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
-
+# TEST SUCCESS/FAILURE ON CREATING/DELETING - VIEWS ASWELL- CHECK PRESENCE OF TEXT IN PAGE
+# NO MODEL TESTING FOR NOW
 class RequestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  test "Get Requests" do
+    sign_in users(:three)
+    get requests_url
+    assert_response :success
+  end
+
+  test "Get Sent Requests" do
+    sign_in users(:four)
+    get requests_sent_url
+    assert_response :success
+  end
 end
