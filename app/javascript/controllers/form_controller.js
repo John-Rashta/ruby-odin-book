e2603db142c6d84b6c1f789f216ff97c4e0dbd67
John-Rashta <john_rashta@outlook.com>
2026-07-14 17:42:05 +0100

    feat: add form disable if every field empty

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "field", "submit" ]
+
+  connect() {
+    this.disableIfEmptyFields();
+  }
+
   clear() {
     this.element.reset();
+  }
+
+  disableIfEmptyFields() {
+    const fieldsEmpty = this.fieldTargets.every(field => {
+      const trixEditor = field.editor;
+      if (trixEditor) {
+        const trixDoc = trixEditor.getDocument()
+        return trixDoc.getAttachments().length === 0 && trixDoc.toString().trim().length === 0
+      } else if (field.type === "file") {
+        return field.files.length === 0;
+      } else {
+        return field.value.trim() === "";
+      };
+    });
+
+    this.submitTarget.disabled = fieldsEmpty;
   }
 }
